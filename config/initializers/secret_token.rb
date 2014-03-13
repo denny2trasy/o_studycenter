50935beed9bb2006cfbabb01dc0a57c0dc30f519
denny2trasy <denny2trasy@gmail.com>
2014-03-13 23:46:50 +0800

    add secret file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# ElStudycenter::Application.config.secret_token = '75deb52873c7ac76865a81fa99efeb8b61835a507356fcc05d919018ce49654ca040f55e709f2ef7bee4d77e5eaccb64882e8fc73110dc0ca9a64cea1552d2f6'
+ElStudycenter::Application.config.secret_token = 'e75165b4c60ec5e5c247c6c406488e68556ea31e50d3d3a5156fb6c9c85a40e4e9e0d29786989e5195741a29c46886605318385dd91605ef69524251020ae952'
